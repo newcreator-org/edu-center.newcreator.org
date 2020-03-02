acd8ac204d3bf3eba659258a74fba6486cdeb527
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-03-02 16:03:27 +0900

    ✨(env): add cz-custom and gulp

---
+++ .cz-config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  types: [
+    { value: '✨', name: 'New function' },
+    { value: '🔧', name: 'bug fix' },
+    { value: '😍', name: 'fix' },
+    { value: '🗑', name: 'remove' },
+    { value: '💪', name: 'other' }
+  ]
+};
+
